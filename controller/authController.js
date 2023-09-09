1735bd4fe332cc10e10e83dc768277b7eb2a2cab
NikhilEvra <nikhil@evramedia.com>
2023-09-09 16:46:16 +0530

    all functions and data working well before devlopment

--- controller/authController.js
+++ controller/authController.js
@@ -15,6 +15,7 @@ exports.signup = catchAsync(async(req, res, next) => {
 
             const rand = Math.floor(Math.random() * 9000 + 1000);
             const phone = Number(req.body.phone);
+            console.log(req.body);
 
             const sql2 = `SELECT COUNT(*) AS count FROM user_master WHERE phone='${phone}'`;  
         
@@ -30,10 +31,7 @@ exports.signup = catchAsync(async(req, res, next) => {
                     const val = [uu_id, phone, rand];
                     
                     con.query(sql3, [val], (err, result3) => {
-                    
-                        if(result3.affectedRows == 0) return next(new AppError('Something went wrong! Insert the records!'))
-                        
-                        
+                  
                         // const req = unirest.post("https://www.fast2sms.com/dev/bulkV2");
                         // req.headers({
                         //     "authorization": "hp6KLBVI8cAvnSu0yMXPk4F17JQUjH9moOC2Dzd3WftqaZsx5lKgEx4zbCIQHPjpnO7AVdsoYRXi2Z15"
@@ -60,12 +58,14 @@ exports.signup = catchAsync(async(req, res, next) => {
     
                 const rand = Math.floor(Math.random() * 9000 + 1000);
                 const phone = Number(req.body.phone);
-    
+    // console.log(phone);
+    console.log(req.body);
+
                 const sql2 = `SELECT COUNT(*) AS count FROM user_master WHERE phone='${phone}'`;  
             
                 con.query(sql2, (err, result2) => {
                     
-                    result2.forEach(el => {
+                    result2.forEach(el => { 
                                            
                         if(el.count > 0){
                             return next(new AppError('Mobile Number Already Regestered!', 400));
@@ -74,9 +74,7 @@ exports.signup = catchAsync(async(req, res, next) => {
                         const sql3 = "INSERT INTO user_master(u_id, phone, otp) VALUES(?)";
                         const val = [uu_id, phone, rand];
                         
-                        con.query(sql3, [val], (err, result3) => {
-                        
-                            if(result3.affectedRows == 0) return next(new AppError('Something went wrong! Insert the records!'))
+                        con.query(sql3, [val], (err, result3) => {                                              
                             
                             // const req = unirest.post("https://www.fast2sms.com/dev/bulkV2");
                             // req.headers({
@@ -91,7 +89,7 @@ exports.signup = catchAsync(async(req, res, next) => {
                             //   req.end(res => {});
                               res.status(200).json({
                                 status: 'success',
-                                message: 'OTP Sent Successfully!'
+                                message: 'OTP Sent Successfully!',                                
                             })
                             
                         })
@@ -108,6 +106,8 @@ exports.signup = catchAsync(async(req, res, next) => {
 exports.login = catchAsync(async(req, res, next) => {
    
     const phone = req.body.phone;
+    console.log(phone)
+
     const sql = `SELECT * FROM user_master WHERE phone='${phone}' AND status='Active'`;
     con.query(sql, (err, result) => {
 
@@ -117,9 +117,9 @@ exports.login = catchAsync(async(req, res, next) => {
 
         const sql2 = `UPDATE user_master SET otp=? WHERE phone='${phone}'`;
         con.query(sql2, rand, (err, result2) => {
-            if(result2.affectedRows == 0) return next(new AppError('Something went wrong!', 400));
 
-            const req = unirest("POST", "https://www.fast2sms.com/dev/bulkV2");
+
+            // const req = unirest("POST", "https://www.fast2sms.com/dev/bulkV2");
             // req.headers({
             //     "authorization": "hp6KLBVI8cAvnSu0yMXPk4F17JQUjH9moOC2Dzd3WftqaZsx5lKgEx4zbCIQHPjpnO7AVdsoYRXi2Z15"
             // });
@@ -140,18 +140,21 @@ exports.login = catchAsync(async(req, res, next) => {
 
 });
 
-exports.otpVerification = catchAsync(async(req, res, next) => {
+exports.otpVerification = catchAsync(async(req, res,next) => {
 
     const phone = req.body.phone;
     const otp = req.body.otp;
+    console.log(phone);
+    console.log(otp);
     $sql = `SELECT * FROM user_master WHERE phone='${phone}' AND otp=${otp} AND status='Active'`;
     con.query($sql, (err, result) => {
         
-        if(result.length == 0) return next(new AppError('Invalid Otp! Please Try Again!'));
+     if(result.length == 0) return next(new AppError('Invalid Otp! Please Try Again!'));
 
         res.status(200).json({
             status : 'success',
-            data : result
+            message : 'Otp validated successfully',
+            data : result 
         })
     })
 
